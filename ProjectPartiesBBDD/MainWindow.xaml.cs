a06feb00cd94da083ff305e7e8c9f581daeab310
Mikel <miguel5121996@gmail.com>
2023-11-11 12:42:07 +0100

    Modifico BBDD para la clase partido, borro  el contenido del boton simulate porque habra que hacerlo diferente

--- ProjectPartiesBBDD/MainWindow.xaml.cs
+++ ProjectPartiesBBDD/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using ProjectPartiesBBDD.ViewModel;
 using ProyectoPoblacion.Domain;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -30,7 +31,6 @@ namespace ProjectPartiesBBDD
         public MainWindow()
         {
             InitializeComponent();
-            List<Partie> parties = modelView.listPart.ToList();
             DataContext = modelView;
             //Cargamos los datos existentes en la BDD
             modelView.LoadUsers();
@@ -38,8 +38,8 @@ namespace ProjectPartiesBBDD
             tvAbsten.TextChanged += calPoblation;
             poblation = new Dates();
             this.DataContext = poblation;
-            dgvParties.ItemsSource = parties.getListPartie();
-            dgvParties2.ItemsSource = parties.getListPartie();
+            dgvParties.ItemsSource = modelView.listPart;
+            dgvParties2.ItemsSource = modelView.listPart;
             txtPresident.TextChanged += txtName_changed;
             txtAcronym.TextChanged += txtName_changed;
             txtName.TextChanged += txtName_changed;
@@ -97,16 +97,6 @@ namespace ProjectPartiesBBDD
                 poblation.calcularValidos(nuV, abs, po);
                 MessageBox.Show("Guardado\n", "Save", MessageBoxButton.OK, MessageBoxImage.Information);
                 Control.SelectedIndex = 1;
-                if (dgvParties.Items.Count == 11)
-                {
-                    int cont = 1;
-                    foreach (Partie partie in parties.getListPartie())
-                    {
-                        parties.calculateValidVotes(partie, cont, poblation.votesValid);
-                        cont++;
-                    }
-                    dgvParties2.Items.Refresh();
-                }
             }
             else
             {
@@ -129,5 +119,96 @@ namespace ProjectPartiesBBDD
             }
 
         }
+        // Guardar partidos
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            if (!(txtAcronym.Text.Equals("") || txtName.Text.Equals("") || txtPresident.Text.Equals("")))
+            {
+
+                if (modelView.listPart == null)
+                {
+                    modelView.listPart = new ObservableCollection<Partie>();
+                }
+                //Para que no agregue un partido igual
+                if (modelView.listPart.Any(p => p.name == txtName.Text))
+                {
+                    MessageBox.Show("Ya existe ese partido", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return; 
+                }
+                modelView.acronym = txtAcronym.Text;
+                modelView.name = txtName.Text;
+                modelView.president = txtPresident.Text;
+                modelView.validVot = 0;
+                modelView.seat = 0;
+
+                // Agregamos nuevo partidp
+                modelView.NewUser();
+
+                txtAcronym.Text = "";
+                txtName.Text = "";
+                txtPresident.Text = "";
+
+                dgvParties.Items.Refresh();
+
+                btnSave.Foreground = new SolidColorBrush(Colors.Black);
+                btnSave.IsEnabled = false;
+
+                // Agregamos votos en blanco si tenemos 10 partidos
+                if (modelView.listPart.Count == 10)
+                {
+                    modelView.acronym = "VB";
+                    modelView.name = "Votos blanco";
+                    modelView.president = "Ninguno";
+                    modelView.validVot = 0;
+                    modelView.seat = 0;
+
+                    // Agregamos votos en blanco a la base de datos
+                    modelView.NewUser();
+                }
+
+                // Como agregamos en la ult posicion votos en blanco tendremos 11 y activara la nueva pestaña
+                if (modelView.listPart.Count == 11)
+                {
+                    Control.SelectedIndex = 2;
+                    item2.IsEnabled = true;
+                    item3.IsEnabled = true;
+                    dgvParties2.Items.Refresh();
+                }
+            }
+        }
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            if (btnDelete.IsEnabled)
+            {
+                // Si no la creamos da error, he encontrado que salta error por un tema de seguridad
+                List<Partie> selectedParties = dgvParties.SelectedItems.Cast<Partie>().ToList();
+
+                foreach (Partie partie in selectedParties)
+                {
+                    modelView.name = partie.name;
+                    modelView.DeleteUser();
+                }
+                // Si existe votos en blanco que lo borre, para que sea en la ultima fila donde se coloque
+                Partie lastParty = modelView.listPart.LastOrDefault();
+                if (lastParty != null && lastParty.name.Equals("Votos blanco"))
+                {
+                    modelView.name = lastParty.name;
+                    modelView.DeleteUser();
+                }
+
+                dgvParties.Items.Refresh();
+
+                if (dgvParties.Items.Count < 11)
+                {
+                    item3.IsEnabled = false;
+                }
+            }
+        }
+        //Boton para simular los escaños
+        private void btnSimulate_(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
     }
 }
--- ProjectPartiesBBDD/ViewModel/UserModelView.cs
+++ ProjectPartiesBBDD/ViewModel/UserModelView.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Data;
+using System.Linq;
 using System.Windows.Controls;
 using ProjectPartiesBBDD.DB;
 using ProjectPartiesBBDD.Domain;
@@ -17,7 +18,7 @@ namespace ProjectPartiesBBDD.ViewModel
         //Declaro la constante para la conexión a la BDD
         private const String cnstr = "server=localhost;uid=miguel;pwd=miguel;database=parties";
         //Modelo de la lista de registros a mostrar
-        private ObservableCollection<Partie> _party;
+        private ObservableCollection<Partie> _listPart;
         private String _acronym = "";
         private String _name = "";
         private String _president = "";
@@ -28,11 +29,11 @@ namespace ProjectPartiesBBDD.ViewModel
         #region OBJETOS
         public ObservableCollection<Partie> listPart
         {
-            get { return _party; }
+            get { return _listPart; }
             set
             {
-                _party = value;
-                OnPropertyChange("party");
+                _listPart = value;
+                OnPropertyChange("listPart");
             }
         }  
         public String acronym
@@ -94,36 +95,51 @@ namespace ProjectPartiesBBDD.ViewModel
             String SQL = $"INSERT INTO party (acronym, name, president, validVot, seat) VALUES ('{acronym}','{name}', '{president}','{validVot}', '{seat}');";
             //usaremos las clases de la librería de MySQL para ejecutar queries
             //Instalar el paqueta MySQL.Data
+
             MySQLDataManagement.ExecuteNonQuery(SQL, cnstr);
+            // Para que cargue si hemos agregao nuevo usuario
+            LoadUsers();
         }
 
         public void UpdateUser()
         {
-            String SQL = $"UPDATE party SET acronym = '{acronym}', president = '{president}, 'validVot = '{validVot}', seat = '{seat}' WHERE name = '{name}';";
+            String SQL = $"UPDATE party SET acronym = '{acronym}', president = '{president}', 'validVot = '{validVot}', seat = '{seat}' WHERE name = '{name}';";
             MySQLDataManagement.ExecuteNonQuery(SQL, cnstr);
         }
         public void DeleteUser()
         {
             String SQL = $"Delete FROM party WHERE name = '{name}';";
             MySQLDataManagement.ExecuteNonQuery(SQL, cnstr);
+            // Para que cargue si hemos borrao nuevo usuario
+            LoadUsers();
         }
 
         public void LoadUsers()
         {
-            String SQL = $"SELECT usuario, mail, edad FROM usuarios;";
+            String SQL = $"SELECT acronym, name, president, validVot, seat FROM party;";
             DataTable dt = MySQLDataManagement.LoadData(SQL, cnstr);
+            if (listPart == null)
+            {
+                listPart = new ObservableCollection<Partie>();
+            }
+            else
+            {
+                // Le faltaba limpiar la lista para que cuando llamemos no duplique la lista 
+                listPart.Clear();
+            }
             if (dt.Rows.Count > 0)
             {
                 if (listPart == null) listPart = new ObservableCollection<Partie>();
+                // Cambiamos para recorrer las filas, y asiganr segun la posicion donde este
                 foreach (DataRow i in dt.Rows)
                 {
                     listPart.Add(new Partie
                     {
                         acronym = i[0].ToString(),
-                        name = i[0].ToString(),
-                        president = i[0].ToString(),
-                        validVot = int.Parse((string)i[0]),
-                        seat = int.Parse((string)i[0])
+                        name = i[1].ToString(),
+                        president = i[2].ToString(),
+                        validVot = int.Parse(i[3].ToString()),
+                        seat = int.Parse(i[4].ToString())
                     });
                 }
             }
